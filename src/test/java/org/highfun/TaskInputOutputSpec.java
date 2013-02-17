46fcefacf885021984b9a0cac812aa39bfc505f5
Piyush <corporate.piyush@gmail.com>
2013-02-17 09:47:41 +0000

    spec for TaskIO
    spec for TaskIO

---
+++ src/test/java/org/highfun/TaskInputOutputSpec.java
@@ -0,0 +1,23 @@
+package org.highfun;
+
+
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
+public class TaskInputOutputSpec {
+
+    @Test
+    public void test(){
+        TaskInputOutput<String, Date> today = new TaskInputOutput<String, Date>("today");
+        assertEquals(today.getInput(), "today");
+        assertEquals(today.getOutput(), null);
+
+        Date date = new Date();
+        today.setOutput(date);
+        assertEquals(today.getInput(), "today");
+        assertEquals(today.getOutput(), date);
+    }
+}
