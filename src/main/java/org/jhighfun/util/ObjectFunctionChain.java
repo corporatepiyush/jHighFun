b2ee8477ba45a128e9dee31a7ce3bbf8966fa415
Piyush <corporate.piyush@gmail.com>
2013-04-07 17:52:49 +0100

    function chaining for object

---
+++ src/main/java/org/jhighfun/util/ObjectFunctionChain.java
@@ -0,0 +1,65 @@
+package org.jhighfun.util;
+
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ObjectFunctionChain<I> {
+
+    private I object;
+
+    public ObjectFunctionChain(I object) {
+        this.object = object;
+    }
+
+    public <O> ObjectFunctionChain<O> transform(Converter<I, O> converter) {
+        return new ObjectFunctionChain<O>(converter.convert(object));
+    }
+
+    public CollectionFunctionChain<I> toCollection() {
+        List<I> collection = new LinkedList<I>();
+        collection.add(object);
+        return new CollectionFunctionChain<I>(collection);
+    }
+
+    public ObjectFunctionChain<I> execute(final Task<I> task) {
+        task.execute(object);
+        return this;
+    }
+
+    public ObjectFunctionChain<I> executeAsync(final Task<I> task) {
+        FunctionUtil.executeAsync(new Block() {
+            public void execute() {
+                task.execute(object);
+            }
+        });
+        return this;
+    }
+
+    public ObjectFunctionChain<I> executeLater(final Task<I> task) {
+        FunctionUtil.executeLater(new Block() {
+            public void execute() {
+                task.execute(object);
+            }
+        });
+        return this;
+    }
+
+    public ObjectFunctionChain<I> executeWithGlobalLock(final Task<I> task) {
+        FunctionUtil.executeWithGlobalLock(new Block() {
+            public void execute() {
+                task.execute(object);
+            }
+        });
+        return this;
+    }
+
+    public I extract() {
+        return object;
+    }
+
+    @Override
+    public String toString() {
+        return object.toString();
+    }
+}
---
+++ src/test/java/org/jhighfun/util/ObjectFunctionChainSpec.java
@@ -0,0 +1,162 @@
+package org.jhighfun.util;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ObjectFunctionChainSpec {
+
+    @Spy
+    ExecutorService spyHighPriorityTaskThreadPool = new ThreadPoolExecutor(1, 100, 1, TimeUnit.SECONDS, new SynchronousQueue<Runnable>());
+
+    @Spy
+    ExecutorService spyMediumPriorityAsyncTaskThreadPool = new ThreadPoolExecutor(0, 100, 1, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+
+    @Spy
+    ExecutorService spyLowPriorityAsyncTaskThreadPool = new ThreadPoolExecutor(0, 5, 1, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+
+
+    @Before
+    public void before() {
+        try {
+            Field globalPool = FunctionUtil.class.getDeclaredField("highPriorityTaskThreadPool");
+            globalPool.setAccessible(true);
+            globalPool.set(null, spyHighPriorityTaskThreadPool);
+
+            globalPool = FunctionUtil.class.getDeclaredField("mediumPriorityAsyncTaskThreadPool");
+            globalPool.setAccessible(true);
+            globalPool.set(null, spyMediumPriorityAsyncTaskThreadPool);
+
+            globalPool = FunctionUtil.class.getDeclaredField("lowPriorityAsyncTaskThreadPool");
+            globalPool.setAccessible(true);
+            globalPool.set(null, spyLowPriorityAsyncTaskThreadPool);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void testExtract(){
+        Object object = new Object();
+        assertTrue(new ObjectFunctionChain<Object>(object).extract()== object);
+    }
+
+    @Test
+    public void testTransform(){
+        Object object = new Object();
+        ObjectFunctionChain<Object> chain = new ObjectFunctionChain<Object>(object);
+
+        Converter mockConverter = mock(Converter.class);
+        chain.transform(mockConverter);
+        verify(mockConverter, times(1)).convert(object);
+
+    }
+
+    @Test
+    public void testToCollection(){
+        Object object = new Object();
+        ObjectFunctionChain<Object> objectFunctionChain = new ObjectFunctionChain<Object>(object);
+
+        CollectionFunctionChain<Object> collectionFunctionChain = objectFunctionChain.toCollection();
+
+        Collection<Object> objectCollection = collectionFunctionChain.extract();
+
+        assertTrue(objectCollection.size()==1);
+        assertTrue(objectCollection.contains(object));
+    }
+
+    @Test
+    public void testExecute(){
+
+        Object object = new Object();
+        ObjectFunctionChain<Object> objectFunctionChain = new ObjectFunctionChain<Object>(object);
+        Task<Object> mockTask = mock(Task.class);
+        objectFunctionChain.execute(mockTask);
+
+        verify(mockTask, times(1)).execute(object);
+
+    }
+
+    @Test
+    public void testExecuteAsync(){
+
+        ObjectFunctionChain<String> chain = new ObjectFunctionChain<String>("Scala");
+        Task<String> mockTask = mock(Task.class);
+
+        chain.executeAsync(mockTask);
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        verify(mockTask, times(1)).execute("Scala");
+        verify(spyMediumPriorityAsyncTaskThreadPool, times(1)).submit(any((Runnable.class)));
+    }
+
+    @Test
+    public void testExecuteLater(){
+
+        ObjectFunctionChain<String> chain = new ObjectFunctionChain<String>("Scala");
+        Task<String> mockTask = mock(Task.class);
+
+        chain.executeLater(mockTask);
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        verify(mockTask, times(1)).execute("Scala");
+        verify(spyLowPriorityAsyncTaskThreadPool, times(1)).submit(any((Runnable.class)));
+    }
+
+    @Test
+    public void testExecuteWithGlobalLockWithMultipleThread() {
+
+        final List<Integer> list = new LinkedList<Integer>();
+
+        List<Integer> load = CollectionUtil.List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        for (int i = 0; i < 10000; list.add(i), i++) ;
+
+        final Block spyBlock = spy(new Block() {
+            public void execute() {
+                list.add(1);
+                for (Integer i : list) ;
+                list.add(2);
+            }
+        });
+
+        FunctionUtil.each(load, new RecordProcessor<Integer>() {
+            public void process(Integer item) {
+                new ObjectFunctionChain<Integer>(1).executeWithGlobalLock(new Task<Integer>() {
+                    public void execute(Integer input) {
+                        spyBlock.execute();
+                    }
+                });
+            }
+        }, FunctionUtil.parallel(10));
+
+        verify(spyBlock, times(10)).execute();
+    }
+}
