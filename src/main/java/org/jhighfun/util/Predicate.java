fc54e74cb24b5c6970c2a331877d3a39ea4e129c
Piyush <corporate.piyush@gmail.com>
2013-05-06 14:04:50 +0100

    default predicates

--- src/main/java/org/jhighfun/util/Predicate.java
+++ src/main/java/org/jhighfun/util/Predicate.java
@@ -2,6 +2,18 @@ package org.jhighfun.util;
 
 public interface Predicate<T> {
 
+    static Predicate TRUE = new Predicate() {
+        public boolean evaluate(Object o) {
+            return true;
+        }
+    };
+
+    static Predicate FALSE = new Predicate() {
+        public boolean evaluate(Object o) {
+            return false;
+        }
+    };
+
     public boolean evaluate(T t);
 
 }
